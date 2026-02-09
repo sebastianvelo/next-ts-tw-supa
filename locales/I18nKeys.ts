562ef42b0453e3ecd3e85a38f8699086a93e1949
Sebastian <sebastian.velo@mercadolibre.com>
2026-02-09 09:25:53 -0300

    feat: Add a new `Brand` component and aggregate i18n keys into a central `I18n` object.

--- locales/I18nKeys.ts
+++ locales/I18nKeys.ts
@@ -1,9 +1,11 @@
 import FORM from "./app/form/wordings";
+import NAVBAR from "./app/layout/navbar";
 import LOADING from "./app/layout/loading";
 import LOGIN from "./app/login/wordings";
 import USER from "./app/user/wordings";
 
 const I18n = {
+    NAVBAR,
     FORM,
     LOGIN,
     LOADING,
